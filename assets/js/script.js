38567f29699bbe7633d3863e2bcf8f81b8766dd2
Jon Gregory <jongregory75@hotmail.com>
2021-05-03 21:32:54 -0600

    API Calls finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,75 @@
-//declare variables to display today, today + 1, etc
-
-//var todayDay = moment .date .day of year .name
-//Card header is today
-//card header tomorrow = todayDay +1 = moment .date .day of year .name
-var apiKey = "05a905770df38bb47d2a0c4f7c95efa9";
-var cityButton = document.getElementById("btn-city-select");
-
-var requestUrl =
-  "https://api.github.com/orgs/nodejs/repoapi.openweathermap.org/data/2.5/weather?q={Denver}&appid={" +
-  apiKey +
-  "}s";
-//
-var apiKey = "05a905770df38bb47d2a0c4f7c95efa9";
+var apiKey = "b332282f910be7fa8ffff49f3e6cd500";
+//var cityButton = document.getElementById("btn-city-select");
+var coord = "";
+var city = "";
+var long = "";
+var lat = "";
+var oneCallUrl = "";
+var baseUrl = "http://api.openweathermap.org/data/2.5/";
+var cityUrl = "";
 
 $(document).ready(function () {
   $("#currentDay").text(moment().format("dddd, MMMM Do  YYYY"));
 
-  var city = "";
-  var long = "";
-  var lat = "";
+  $(".btn-city-select").on("click", function () {
+    city = $(this).siblings(".cityNameInput").val();
+    console.log(city);
+    var cityName = "cityName";
+    cityValue = city;
 
-  function checkCity() {
-    // fetch request gets a list of all the repos for the node.js organization
+    //put city input into local storage
+    localStorage.setItem(cityName, JSON.stringify(cityValue));
+    buildCityUrl(city);
+  });
 
-    //API Call for initial city weather
-    //api.openweathermap.org/data/2.5/weather?q={city name}&appid={apiKey}
+  //var citySelectBtn = document.getElementById("btn-city-select");
+  //var citySelectVal = document.getElementById("cityNameInput");
+  //   var btn = document.getElementById("btn-city-select");
+  //   btn.setAttribute("btn-city-select", searchHistory[i]);
+  //   console.log(btn.textContent);
+  //   btn.textContent = searchHistory[i];
+  //   city = btn.textContent;
+  //   searchHistoryContainer.append(btn);
 
-    //parse return for long / lat
+  //citySelectBtn.onclick = buildCityUrl(citySelectVal);
 
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
+  function buildCityUrl(city) {
+    var cityUrl = baseUrl + "weather?q=" + city + "&appid=" + apiKey;
 
-      .then(function (data) {
-        console.log(data);
-        //Loop over the data to generate a table, each table row will have a link to the repo url
-        //for (var i = 0; i < data.length; i++) {
-        // Creating data poins from object returned
-      });
-
-    //URLfor One Call API
-    //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+    checkCity(cityUrl);
   }
 
-  cityButton.addEventListener("click", checkCity);
+  function checkCity(cityUrl) {
+    fetch(cityUrl)
+      .then((response) => response.json())
+
+      .then(function (data) {
+        grabLatLon(data);
+        console.log(data);
+      });
+  }
+
+  function grabLatLon(dataIn) {
+    long = dataIn.coord.lon;
+    lat = dataIn.coord.lat;
+
+    oneCallUrl =
+      baseUrl + "onecall?lat=" + lat + "&lon=" + long + "&appid=" + apiKey;
+    console.log(oneCallUrl);
+    getOneCallApi(oneCallUrl);
+  }
+
+  //calls the One Call API Url and returns the One Call Data
+  function getOneCallApi(oneCallUrl) {
+    fetch(oneCallUrl)
+      .then((response) => response.json())
+
+      .then(function (oneCallData) {
+        console.log(oneCallData);
+      });
+  }
 });
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+}
